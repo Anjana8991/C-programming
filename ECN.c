a8fc726ce8efc27a1c295798f5c424606c79e4a4
anjanadw <anjana@digitalwaters.org>
2024-10-24 11:37:50 -0400

    Added pointer basics  program

--- ECN.c
+++ ECN.c
@@ -1,19 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
- 
- float *arr=(float *)malloc(5*sizeof(float));
- arr[0]=1.1;
-  arr[1]=2.1;
-   arr[2]=3.1;
-    arr[3]=4.1;
-     arr[4]=5.1;
-  
-for(int i=0;i<5;i++)
- printf("The values are %f \n",arr[i]);
+    int n;
+int *arr;
+printf("Enter the number that need to be stored\n");
+scanf("%d",&n);
+int *arr=(int *)malloc(n*sizeof(int));
 
-free (arr);
-return 0;
+
 
 }
 
--- hello.c
+++ hello.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int temp;
+    int A[10];
+    printf("Enter the array elements\n");
+   for(int i=0;i<10;i++){
+     scanf("%d",&A[i]);
+   }
+   
+    int *a;
+    int *b;
+    a=A;
+    b=&A[9];//A+9
+while (a<b)
+{
+  temp=*a;
+  *a=*b;
+  *b=temp;
+  a++;
+  b--;
+    /* code */
+}
+for(int i=0;i<10;i++){
+printf("Reversed array%d\n",A[i]);
+}
+return 0;
 }
