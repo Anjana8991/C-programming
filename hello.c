bcb6d64a4fdbb6e6ca89a950e5be14608a8da150
anjanadw <anjana@digitalwaters.org>
2024-10-25 20:57:32 -0400

    Added pointer basics_find largest in an array  program

--- hello.c
+++ hello.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int temp;
+    int *a,largest;
     int A[10];
-    printf("Enter the array elements\n");
-   for(int i=0;i<10;i++){
+     printf("Enter the elemnts\n");
+     for(int i=0;i<10;i++){
      scanf("%d",&A[i]);
-   }
-   
-    int *a;
-    int *b;
+     }
     a=A;
-    b=&A[9];//A+9
-while (a<b)
-{
-  temp=*a;
-  *a=*b;
-  *b=temp;
-  a++;
-  b--;
-    /* code */
-}
-for(int i=0;i<10;i++){
-printf("Reversed array%d\n",A[i]);
-}
-return 0;
-}
+    largest=*a;
+   
+    for(int i=0;i<10;i++){
+        if(*a>largest){
+            largest =*a;
+        }
+        a++;
+    }
+    printf(" the largest element is%d \n",largest);
+
+
+   }
+
+
+
+
