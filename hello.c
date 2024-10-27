707cb92463b608db453f0f7e3fdace292c49361e
anjanadw <anjana@digitalwaters.org>
2024-10-27 18:22:21 -0400

    Guessing game

--- hello.c
+++ hello.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-   
-    int A[]={2,4,6,8,10};
-     int *p;
+int *p;
+int *q;
     int i;
 
  p=(int*)malloc(5*sizeof(int));
+ 
+ q=(int*)malloc(10*sizeof(int));
  p[0]=3;
  p[1]=5;
  p[2]=7;
  p[3]=9;
  p[4]=11;
 for(i=0;i<5;i++){
-    printf("%d ",A[i]);
-}
-printf("\n");
-for(i=0;i<5;i++){
-    printf("%d ",p[i]);
+    q[i]=p[i];
+   free(p);  
 }
 
- free(p);
+printf("\n");
+p=q;
+q=NULL;
+for(i=0;i<5;i++){
+        printf("%d ",q[i]);
+}
+
 return 0;
 }
 
