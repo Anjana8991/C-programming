cead65661159c9a72dcd78dd6fc01c78bf6bb1b8
anjanadw <anjana@digitalwaters.org>
2024-10-27 19:42:36 -0400

    Digital clock project

---
+++ C_programming/ECN.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int n;
+int *arr;
+printf("Enter the number that need to be stored\n");
+scanf("%d",&n);
+int *arr=(int *)malloc(n*sizeof(int));
+
+
+
+}
+
+  
